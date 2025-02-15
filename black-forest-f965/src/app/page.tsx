fd2e6e363e96168dc27c7198be0be5b9bbb705ce
Akilan Purushothaman <akilp@Akilans-MacBook-Pro.local>
2025-02-15 10:41:51 -0800

    fix

--- black-forest-f965/src/app/page.tsx
+++ black-forest-f965/src/app/page.tsx
@@ -2,23 +2,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <!doctype html>
-<html>
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
-</head>
-<body>
-    <h1 class="text-yellow font-mono">Enter your tasks below:</h1>
+  <div>
+    <h1 className="text-yellow font-mono">Enter your tasks below:</h1>
     <h2>Task Input</h2>
     <form>
-        <label for="task">Please enter you first task name: </label><br>
-        <input type="text" id="task" name="task"><br>
-        <input type="submit" value="Submit">
+        <label htmlFor="task">Please enter you first task name: </label>
+        <input type="text" id="task" name="task"/>
+        <input type="submit" value="Submit"/>
         <form action="/action_page.php" method="get"></form>
     </form>
-</body>
-</html>
+
+</div>
   );
 }
