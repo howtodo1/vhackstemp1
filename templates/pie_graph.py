fa4da51eafe7c997159906865c182c1b570e241a
Vishv Dhanraj <vishvdhanraj@Vishvs-MacBook-Air.local>
2025-02-15 12:16:19 -0800

    vishva

--- templates/pie_graph.py
+++ templates/pie_graph.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
-def pie_chart(class1, class2, class3, class4):
-    time_spent = [class1, class2, class3, class4]
+def pie_chart(time_spent): # time_spent is a list:  time_spent = [class1, class2, class3, class4, ...]
     totalTime = 0
     for classNum in time_spent:
         totalTime += classNum
@@ -8,13 +7,16 @@ def pie_chart(class1, class2, class3, class4):
     print (average)
     piePercent = []
     for classNum2 in time_spent:
-        piePercent.insert(1, classNum2/totalTime)
+        piePercent.append(classNum2/totalTime)
 
     labels = ['class1', 'class2', 'class3', 'class4']
     colors = ['red', 'yellow', 'pink', 'brown']  
     plt.pie(piePercent, labels=labels, colors=colors, autopct='%1.1f%%', startangle=140)
     plt.title("% time spent on each subject")
     plt.show()
+
+if __name__ == "__main__":
+    pie_chart([3,4,2,1])
 
 
 
