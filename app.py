b9bcf936f137a3e96e5621b58fce387e614fc629
Vishv Dhanraj <vishvdhanraj@Vishvs-MacBook-Air.local>
2025-02-15 11:45:04 -0800

    vishva

--- app.py
+++ app.py
@@ -11,6 +11,5 @@ def hello_world():
 # time is amount of time taken to complete task, 
 # difficulty is the amount of effort needed, from 1 to 10
 # importance is the urgency of the task, from 1 to 10
-def importance_calc(time, difficulty, importance)
+def importance_calc(time, difficulty, importance):
     taskList.insert([time, difficulty, importance])
-    
