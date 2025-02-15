fcc3682d2bde7b232e2889b12d26147c2f9a54f9
Vishv Dhanraj <vishvdhanraj@Vishvs-MacBook-Air.local>
2025-02-15 12:32:18 -0800

    created pie graph code

--- templates/pie_graph.py
+++ templates/pie_graph.py
@@ -15,6 +15,12 @@ def pie_chart(time_spent): # time_spent is a list:  time_spent = [class1, class2
     plt.title("% time spent on each subject")
     plt.show()
 
+def line_graph(last7days): #last7days is a list of the time spent on each for past 7 days 
+    #last7days = [[class1, class2, class3, ...], [], [], []] where each [] is a list of time spent each day
+    
+    print()
+
+
 if __name__ == "__main__":
     pie_chart([3,4,2,1])
 
