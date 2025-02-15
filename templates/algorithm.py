4474ba14c9dd8b3dcb8e4143a6652ce3ab8acf47
Vishv Dhanraj <vishvdhanraj@Vishvs-MacBook-Air.local>
2025-02-15 10:29:08 -0800

    vishva

--- templates/algorithm.py
+++ templates/algorithm.py
@@ -1,13 +1,5 @@
-from flask import render_template, Flask
-
-app = Flask(__name__)
 
 taskList = []
-
-@app.route("/")
-def hello_world():
-    return render_template('index.html')
-
 # time is amount of time taken to complete task, 
 # difficulty is the amount of effort needed, from 1 to 10
 # importance is the urgency of the task, from 1 to 10
