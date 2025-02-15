fd3cda11f3878e427322c3d627c71b2a7765554c
Vishv Dhanraj <vishvdhanraj@Vishvs-MacBook-Air.local>
2025-02-15 14:37:59 -0800

    vishva

--- templates/pie_graph.py
+++ templates/pie_graph.py
@@ -10,7 +10,7 @@ def pie_chart(time_spent): # time_spent is a list:  time_spent = [class1, class2
         piePercent.append(classNum2/totalTime)
 
     labels = ['class1', 'class2', 'class3', 'class4']
-    colors = ['red', 'yellow', 'pink', 'brown']  
+    colors = ['red', 'green', 'blue', 'yellow']  
     plt.pie(piePercent, labels=labels, colors=colors, autopct='%1.1f%%', startangle=140)
     plt.title("% time spent on each subject")
     plt.show()
@@ -23,7 +23,12 @@ def line_graph(last7days): #last7days is a list of the time spent on each for pa
 
 
 if __name__ == "__main__":
-    pie_chart([3,4,2,1])
+    #pie_chart([3,4,2,1])
+    t1 =  int(input("How much for first one? >>> "))
+    t2 =  int(input("How much for second one? >>> "))
+    t3 =  int(input("How much for third one? >>> "))
+    t4 =  int(input("How much for fourth one? >>> "))
+    pie_chart([t1, t2, t3, t4])
 
 
 
